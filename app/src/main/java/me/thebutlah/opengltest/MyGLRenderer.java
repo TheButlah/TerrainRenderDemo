281a137c40c0a2a760827d578e3dad91183cb141
Ryan Butler <thebutlah@gmail.com>
2015-12-23 16:54:20 -0600

    THE PROGRAM WORKS ERRMAGHEERDgit add *! YAY!

--- app/src/main/java/me/thebutlah/opengltest/MyGLRenderer.java
+++ app/src/main/java/me/thebutlah/opengltest/MyGLRenderer.java
@@ -7,6 +7,9 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -14,7 +17,16 @@ import javax.microedition.khronos.opengles.GL10;
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     private final Context context;
+
     private ShaderProgram shaderProgram;
+    //an array is used for two reasons: because there can be more than one buffer if desired, and because arrays are pass by reference
+    private final int[] vboHandle = new int[1];
+
+    private final float[] vertices = {
+            0.0f,  0.5f,  0.0f,  1.0f, //each line represents (x,y,z,w) of a single vertex. This, for example, is the top of the triangle
+            -0.5f,  0.0f,  0.0f,  1.0f,
+            0.5f,  0.0f,  0.0f,  1.0f
+    };
 
     public MyGLRenderer(Context context) {
         this.context = context;
@@ -36,11 +48,30 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
                 Log.e(MainActivity.LOGGER_TAG, "Could not close one or more shader streams! However, we have already loaded the shaders so it doesn't really matter!", e);
             }
             shaderProgram = new ShaderProgram(basicVertexShader, basicFragmentShader);
-        }//End Initialize Shaders
+        }
+
+        {//Set up VBOs
+            Log.v(MainActivity.LOGGER_TAG, "Initializing VBOs");
+            //Represents the vertex data, but stored in a native buffer in RAM. Note that this is not a VBO.
+            //vertices.length * 4 because 1 float = 4 bytes
+            FloatBuffer nativeVertices = ByteBuffer.allocateDirect(vertices.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+            //load the vertex data from the Java heap into the native heap
+            nativeVertices.put(vertices);
+            //set the pointer to the data as the first element
+            nativeVertices.position(0);
+
+            //Get the handle for a VBO, store it in vboHandle
+            GLES20.glGenBuffers(1, vboHandle, 0);
+            //Set the VBO as active, future calls will be referring to it.
+            GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboHandle[0]);
+            //Actually load the VBO, this places the data onto the GPU memory instead of RAM. The native and Java data can now be GCed
+            GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, 4 * nativeVertices.capacity(), nativeVertices, GLES20.GL_STATIC_DRAW);
+            //Unbind the VBO. Think of this as setting the VBO that is currently active to null. Future OpenGL calls will now not affect VBO.
+            GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER,0);
+        }
 
 
-
-
+        Log.v(MainActivity.LOGGER_TAG, "Finished setting up VBOs!");
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
@@ -49,9 +80,27 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        //Use the shader program
         GLES20.glUseProgram(shaderProgram.programID);
+        //Bind the VBO
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboHandle[0]);
 
+        //Get a handle for the variable "vPosition" within the shader program. This will be used to reference that attribute.
+        int positionAttrib = GLES20.glGetAttribLocation(shaderProgram.programID, "vPosition");
+        //Enable the attribute. Unsure why this is really needed, but it is so don't question it.
+        GLES20.glEnableVertexAttribArray(positionAttrib);
 
+        //Tell the attribute how the VBO is formatted
+        GLES20.glVertexAttribPointer(positionAttrib, 4, GLES20.GL_FLOAT, false, 0, 0);
+
+        //Draw effyching
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertices.length);
+
+        //Disable the attribute. Again, don't understand purpose.
+        GLES20.glDisableVertexAttribArray(positionAttrib);
+        //Unbind the VBO, although in this usage case it isnt even necessary.
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
     }
 
     @Override
--- app/src/main/res/raw/basic_fragmentshader.glsl
+++ app/src/main/res/raw/basic_fragmentshader.glsl
@@ -1,6 +1,6 @@
-precision mediump float;
-uniform vec4 vColor;
+//precision mediump float;
+//uniform vec4 vColor;
 
 void main() {
-    gl_FragColor = vColor;
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }
